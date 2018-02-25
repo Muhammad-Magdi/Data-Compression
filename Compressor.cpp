ff9e13d86eff37cd9cc4177eb1341dfcbe959904
TripleM96 <mohamedmamdouh1996@hotmail.com>
2018-02-25 04:22:13 +0200

    Update Compressor.cpp

--- Compressor.cpp
+++ Compressor.cpp
@@ -6,12 +6,38 @@ Compressor :: Compressor(string IF){
 	inputFile = IF;
 }
 
-void Compressor :: deleteSpaces(){
-	vector<string> 
+void Compressor :: deleteSpaces(string IF){
+	vector<string> tree;
+	string outputFile = "";
+	for(int i=0; i<inputFile.size(); i++){
+    if(inf[i]=='<'){
+      while(inf[i]!='>'){
+        outputFile.push_back(inf[i++]);
+      }
+      outputFile.push_back()
+      continue;
+    }
+    if(inf[i]=='"'){
+      while(inf[i]!='"'){
+        outputFile.push_back(inf[i++]);
+      }
+      outputFile.push_back()
+      continue;
+    }
+    if(inf[i]=='\''){
+      while(inf[i]!='\''){
+        outputFile.push_back(inf[i++]);
+      }
+      outputFile.push_back()
+      continue;
+    }
+    if(inf[i] == ' ') continue;
+    outputFile.push_back(inf[i]);
+	}
 }
 
 bool Compressor :: compress(string IF){
-	if(IF.empty() && inputFile.empty())	return 0;	
+	if(IF.empty() && inputFile.empty())	return 0;
 }
 
 bool Compressor :: deCompress(string IF){
